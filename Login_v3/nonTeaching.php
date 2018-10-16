5c3e3e6a6f8aa35111d6f54a6dd5e45e352b95a6
Alex Yip <yiucheung1999@gmail.com>
2018-10-16 11:00:10 +0800

    Enter

--- Login_v3/nonTeaching.php
+++ Login_v3/nonTeaching.php
@@ -1,77 +1,101 @@
-<!DOCTYPE html>
-<html>
-<head>
-      <title>Library System: Staff Page</title>
-   	<link rel="stylesheet" type="text/css" href="css/mainpage.css">
+< !DOCTYPE html >
+    <
+    html >
+    <
+    head >
+    <
+    title > Library System: Staff Page < /title> <
+    link rel = "stylesheet"
+type = "text/css"
+href = "css/mainpage.css" >
 
-    <script>
-        /* When the user clicks on the button, 
-                toggle between hiding and showing the dropdown content */
-        function showdropdown(id) {
-            document.getElementById(id).classList.toggle("show");
-        }
+    <
+    script >
+    /* When the user clicks on the button, 
+            toggle between hiding and showing the dropdown content */
+    function showdropdown(id) {
+        document.getElementById(id).classList.toggle("show");
+    }
 
-        // Close the dropdown if the user clicks outside of it
-        window.onclick = function(e) {
-            if (!e.target.matches('.dropbtn')) {
-                var myDropdown = document.getElementById("myDropdown");
-                var MenuDropdown = document.getElementById("MenuDropdown");
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (e) {
+        if (!e.target.matches('.dropbtn')) {
+            var myDropdown = document.getElementById("myDropdown");
+            var MenuDropdown = document.getElementById("MenuDropdown");
 
-                if (myDropdown.classList.contains('show')) {
-                    myDropdown.classList.remove('show');
-                }
+            if (myDropdown.classList.contains('show')) {
+                myDropdown.classList.remove('show');
+            }
 
-                if (MenuDropdown.classList.contains('show')) {
-                    MenuDropdown.classList.remove('show');
-                }
+            if (MenuDropdown.classList.contains('show')) {
+                MenuDropdown.classList.remove('show');
             }
         }
-    </script>
-</head>
+    } <
+    /script> <
+    /head>
 
-<body>
-<?php
+    <
+    body >
+    <?php
     if(isset($_SERVER["QUERY_STRING"])){
         extract($_GET);
-
-        if($password=="nonTeaching"){
-            header("Location: changePassword.php?username=nonTeaching");
-        }
+        if(isset($password)){
+            if($password=="nonTeaching"){
+                header("Location: changePassword.php?username=nonTeaching");
+            }
+    }
     }
 
     ?>
 
-    <div class="navbar">
-        <a href="#home">Home</a>
-        <a href="#news">News</a>
+    <
+    div class = "navbar" >
+    <
+    a href = "#home" > Home < /a> <
+    a href = "#news" > News < /a>
 
-        <div class="dropdown">
-            <button class="dropbtn" onclick="showdropdown('myDropdown')">Dropdown
-                <i class="fa fa-caret-down"></i>
-            </button>
-            <div class="dropdown-content" id="myDropdown">
-                <a href="#">Link 1</a>
-                <a href="#">Link 2</a>
-                <a href="#">Link 3</a>
-            </div>
-        </div>
-
-
-        <div class="dropdown" style="float:right">
-            <button class="dropbtn" onclick="showdropdown('MenuDropdown')">Menu
-                <i class="fa fa-caret-down"></i>
-            </button>
-            <div class="dropdown-content" id="MenuDropdown">
-                <a href="#">Link 1</a>
-                <a href="#">Link 2</a>
-                <a href="#">Link 3</a>
-            </div>
-        </div>
-        
-        <a style="float:right">Staff Name: Leung Gor</a>
-
-    </div>
+    <
+    div class = "dropdown" >
+    <
+    button class = "dropbtn"
+onclick = "showdropdown('myDropdown')" > Dropdown <
+    i class = "fa fa-caret-down" > < /i> <
+    /button> <
+    div class = "dropdown-content"
+id = "myDropdown" >
+    <
+    a href = "#" > Link 1 < /a> <
+    a href = "#" > Link 2 < /a> <
+    a href = "#" > Link 3 < /a> <
+    /div> <
+    /div>
 
 
-</body>
-</html>
+    <
+    div class = "dropdown"
+style = "float:right" >
+    <
+    button class = "dropbtn"
+onclick = "showdropdown('MenuDropdown')" > Menu <
+    i class = "fa fa-caret-down" > < /i> <
+    /button> <
+    div class = "dropdown-content"
+id = "MenuDropdown" >
+    <
+    a href = "#" > Link 1 < /a> <
+    a href = "#" > Link 2 < /a> <
+    a href = "#" > Link 3 < /a> <
+    /div> <
+    /div>
+
+    <
+    a style = "float:right" > Staff Name: Leung Gor < /a>
+
+    <
+    /div>
+
+
+    <
+    /body> <
+    /html>
