dc7c26ed71e52b5de62daf46e6bb0c5e8f8680e4
Alex Yip <yiucheung1999@gmail.com>
2018-10-17 13:23:13 +0800

    Update Login_v3/student.php

--- Login_v3/student.php
+++ Login_v3/student.php
@@ -4,9 +4,9 @@
 <head>
     <title>Library System: Student Page</title>
     <link rel="stylesheet" type="text/css" href="css/mainpage.css">
-    <link rel="stylesheet" type="text/css" href="css/fontSize.css"> 
-    <link rel="stylesheet" type="text/css" href="css/Search.css"> 
-        <link rel="stylesheet" type="text/css" href="css/advancedForm.css"> 
+    <link rel="stylesheet" type="text/css" href="css/fontSize.css">
+    <link rel="stylesheet" type="text/css" href="css/Search.css">
+    <link rel="stylesheet" type="text/css" href="css/advancedForm.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="jslib/jquery-1.11.1.js"></script>
     <script>
@@ -114,7 +114,7 @@ function topFunction() {
     }
 
     ?>
-    
+
 
     <div class="navbar">
         <a href="#home">Home</a>
@@ -125,15 +125,15 @@ function topFunction() {
             <button class="dropbtn" onclick="showdropdown('CategoryDropdown')">Category
                 <i class="fa fa-caret-down"></i>
             </button>
-            <div class="dropdown-content" id="CategoryDropdown" >
+            <div class="dropdown-content" id="CategoryDropdown">
                 <a href="#">Books</a>
                 <a href="#">Software</a>
                 <a href="#">Magazines</a>
 
             </div>
         </div>
- 
-            
+
+
 
         <div class="dropdown" style="float:right">
             <button class="dropbtn" onclick="showdropdown('MenuDropdown')">Menu
@@ -147,83 +147,101 @@ function topFunction() {
         </div>
 
         <a style="float:right">Student Name: Yip Yiu Cheung</a>
-   </div>
-    
-        
-  
-           <div  style="background-color: #F5E13C;">
-         <div id="resize">
-              <a class="increase" style="font-size:20px; opacity: 0.9;">A</a> 
-              <a class="decrease" style="font-size:12px; opacity: 0.9;">A</a> 
-              <a class="reset" style="opacity: 0.9;">R</a>
-            </div>
-     <div>
-        <br> 
-         <center>  <form class="example" id="simple">
-                       <br>    <br>    
-            <input type="search" placeholder="Search..."/>
-             <button type="submit" style="margin:auto;max-width:50px"><i class="fa fa-search"></i></button>
-            <button id="search" style="color:white; width: 180px; height: 42px; background-color:light-blue;">Advanced Search </button> 
-         </form></center>
-                   <form  id="advanced" style="display:none;" class="form-container">
-                       <br>    <br>    
-         Search for:   <input type="radio" name="sorting" value="everything"> Everything
-                       <input type="radio" name="sorting" value="books"> Books
-                       <input type="radio" name="sorting" value="software"> Software
-                       <input type="radio" name="sorting" value="Magazines"> Magazines
-                       <br><br>
-                       <hr>
-                       <br>
-                       <select>
-                        <option value="anyfield">Any field</option>
-                        <option value="title">Title</option>
-                        <option value="author">Author</option>
-                        <option value="subject">Subject</option>
-                        <option value="isbn">ISBN</option>
-                      </select> contains
-                       <input type="text"  name="contains" required>
-                       <br>
-                         <select>
-                        <option value="and">AND</option>
-                        <option value="or">OR</option>
-                        <option value="not">NOT</option>
-                      </select> 
-                       
-                        <select>
-                        <option value="anyfield">Any field</option>
-                        <option value="title">Title</option>
-                        <option value="author">Author</option>
-                        <option value="subject">Subject</option>
-                        <option value="isbn">ISBN</option>
-                      </select> contains
-                       <input type="text"  name="contains" required>
-                      <button id="simpleSearch">Simple Search </button> 
-         </form>
-         <br>   
-        </div>
-        
     </div>
- 
-    
-   <section>
-  <nav>
-    <ul>
-      <li><a href="#">London</a></li>
-      <li><a href="#">Paris</a></li>
-      <li><a href="#">Tokyo</a></li>
-    </ul>
-  </nav>
-  
-  <div class="resizable">
-  <article>
-    <img src="jslib/images/1.jpg" title="Book name" style="width:75px; height=100px">
-    <h1>London</h1>
-    London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.
-    Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.<br><br></p>
-    <hr></article>
 
-  </div>    
-</section>
+
+
+    <div style="background-color: #F5E13C;">
+        <div id="resize">
+            <a class="increase" style="font-size:20px; opacity: 0.9;">A</a>
+            <a class="decrease" style="font-size:12px; opacity: 0.9;">A</a>
+            <a class="reset" style="opacity: 0.9;">R</a>
+        </div>
+        <div>
+            <br>
+            <center>
+                <form class="example" id="simple">
+                    <br> <br>
+                    <input type="search" placeholder="Search..." />
+                    <button id="searchBtn" type="button" style="margin:auto;max-width:50px"><i class="fa fa-search"></i></button>
+                    <button id="search" style="color:white; width: 180px; height: 42px; background-color:light-blue;">Advanced
+                        Search </button>
+                </form>
+            </center>
+            <form id="advanced" style="display:none;" class="form-container">
+                <br> <br>
+                Search for: <input type="radio" name="sorting" value="everything"> Everything
+                <input type="radio" name="sorting" value="books"> Books
+                <input type="radio" name="sorting" value="software"> Software
+                <input type="radio" name="sorting" value="Magazines"> Magazines
+                <br><br>
+                <hr>
+                <br>
+                <select>
+                    <option value="anyfield">Any field</option>
+                    <option value="title">Title</option>
+                    <option value="author">Author</option>
+                    <option value="subject">Subject</option>
+                    <option value="isbn">ISBN</option>
+                </select> contains
+                <input type="text" name="contains" required>
+                <br>
+                <select>
+                    <option value="and">AND</option>
+                    <option value="or">OR</option>
+                    <option value="not">NOT</option>
+                </select>
+
+                <select>
+                    <option value="anyfield">Any field</option>
+                    <option value="title">Title</option>
+                    <option value="author">Author</option>
+                    <option value="subject">Subject</option>
+                    <option value="isbn">ISBN</option>
+                </select> contains
+                <input type="text" name="contains" required>
+                <button id="simpleSearch">Simple Search </button>
+            </form>
+            <br>
+        </div>
+
+    </div>
+
+
+    <section>
+        <nav>
+            <ul>
+                <li><a href="#">London</a></li>
+                <li><a href="#">Paris</a></li>
+                <li><a href="#">Tokyo</a></li>
+            </ul>
+        </nav>
+
+        <div class="resizable">
+            <article>
+                <img src="jslib/images/1.jpg" title="Book name" style="width:120px; height=150px; float: left; margin-right: 15px; margin-bottom: 10px;">
+                <h3>Book Name</h3>
+                <table width="600" >
+                    <tr>
+                        <td>Type: Music</td>
+                        <td>Year: 2018</td>
+                        <td>ISBN: 21796247</td>
+                    </tr>
+                    <tr>
+                        <td>Language: English</td>
+                        <td>Author: Author A</td>
+                        <td>Publisher: University of California Press</td>
+                    </tr>
+                </table><br>
+                Description:&emsp;London is the capital city of England. It is the most populous city in the United
+                Kingdom, with a metropolitan area of over 13 million inhabitants.
+                Standing on the River Thames, London has been a major settlement for two millennia, its history going
+                back to its founding by the Romans, who named it Londinium.<br><br>
+                <hr>
+            </article>
+
+        </div>
+    </section>
 
     <img src="arrow.png" onclick="topFunction()" id="myBtn" title="Go to top" style="width:25px; height=25px">
 </body>
